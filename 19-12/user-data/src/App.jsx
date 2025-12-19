48dc3c9e02d2e9ffef3ff0f33a4b131df2232a26
PittaShirisha-hub <pittashirisha@gmail.com>
2025-12-19 19:13:18 +0530

    hi

---
+++ 19-12/user-data/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
